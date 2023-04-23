0c8cab20156006662a6b6bf41c66df0d01e411a6
DenisKostyuk <74106064+DenisKostyuk@users.noreply.github.com>
2023-04-23 20:14:02 +0300

    Update part_two.py

--- part_two.py
+++ part_two.py
@@ -1,74 +1,110 @@
-import io
 import csv
 from PIL import Image, ImageFilter, ImageChops
 import cv2
 import numpy as np
 import argparse
 
-import sys
 
-# Read the image from stdin
+"""
+This is a part where using the terminal you upload a photo by writing the following command :
+python main.py img.png 
+* img.png is the name of the image you wish you upload *
+
+More information about this code : 
+This code plots a csv which writes the coordinates x y and r b , which r is the radiou and b is the brightness 
+"""
 parser = argparse.ArgumentParser(description='Process an image file')
 parser.add_argument('file_path', metavar='FILE', help='the path to the image file')
-
-# Parse the command-line arguments
 args = parser.parse_args()
-
-# Open the image file using PIL
 image = Image.open(args.file_path)
 
-# Convert the image to grayscale
+"""
+Using PIL library, it will transfer the image into grascale. L is the mode of grayscale.
+"""
 gray_image = image.convert("L")
 
-# Apply a threshold to the image
+"""
+Used threshold 175 on grayscale image, we tried more and less , but it worked the best with 175
+each pixel above 175 automaticly will be set to 255
+"""
 threshold = 175
 thresholded_image = gray_image.point(lambda x: 255 if x > threshold else 0)
 
-# Apply a Gaussian blur to the image
+"""
+Linear filter that smooths an image by averaging the pixel values in the neightborhood of each pixel.
+"""
 blurred_image = thresholded_image.filter(ImageFilter.GaussianBlur(radius=5))
 
-# Create a mask for the stars
+"""
+Function from PIL library , and it inverts the colors of an image. It inverts which means each pixel that was 255
+will be 0 now and the oposite.
+"""
 mask = ImageChops.invert(thresholded_image)
 
-# Enhance the brightness of the stars
+"""
+Resulting image will be brighter regions where the features are more pronounced.
+"""
 brightened_image = ImageChops.multiply(blurred_image, mask).point(lambda x: x * 3)
 
-# Convert the PIL image to a numpy array
+"""
+Converts the image object to NumPy array.
+"""
 np_image = np.array(brightened_image)
 
-# Convert the color format to BGR (for OpenCV compatibility)
+"""
+Converts the image color from gray to bgr
+"""
 bgr_image = cv2.cvtColor(np_image, cv2.COLOR_GRAY2BGR)
 
-# Find contours in the brightened image
+"""
+Function that helps to find contours in a binary image.
+return coordinates x,y of the object points
+"""
 contours, hierarchy = cv2.findContours(np_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-# Loop over each contour and calculate the center, brightness, and radius of each star
+"""
+Loop over the contours and draw rectangels in coordinates x y w h
+"""
 stars = []
 for contour in contours:
     # Calculate the bounding rectangle of the contour
     x, y, w, h = cv2.boundingRect(contour)
 
-    # Calculate the center of the contour
+    """
+    center of the contour
+    """
     cx = x + w/2
     cy = y + h/2
 
-    # Calculate the brightness of the contour
+    """
+    brightness of the contour
+    """
     brightness = np.mean(np_image[y:y+h, x:x+w])
 
-    # Calculate the radius of the contour
+    """
+    radious of the contour
+    """
     radius = (w + h)/4
 
-    # Append the x,y coordinates, brightness, and radius to the stars list
+    """
+    appends to array stars 
+    """
     stars.append({'x': cx, 'y': cy, 'brightness': brightness, 'radius': radius})
 
-    # Draw a rectangle around the contour
+    """
+    draws the rectangle
+    """
     cv2.rectangle(bgr_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-# Save the resulting image
+"""
+Saves the image after all the manipulations
+"""
 result_image = Image.fromarray(cv2.cvtColor(bgr_image, cv2.COLOR_BGR2RGB))
-result_image.save('aftermain.jpg')
+result_image.save('aftermain1.jpg')
 
-# Save the star data to a CSV file
+"""
+Saves in a csv all the output x y brightness and radius
+"""
 with open('star_data.csv', mode='w', newline='') as csv_file:
     fieldnames = ['star_number', 'x', 'y', 'brightness', 'radius']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
@@ -77,5 +113,7 @@ with open('star_data.csv', mode='w', newline='') as csv_file:
     for i, star in enumerate(stars):
         writer.writerow({'star_number': i+1, 'x': star['x'], 'y': star['y'], 'brightness': star['brightness'], 'radius': star['radius']})
 
-# Print the number of stars
+"""
+Prints the number of stars that were detected in stars object.
+"""
 print("Number of stars: {}".format(len(stars)))
